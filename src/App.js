c9c23d474b0062d87eab87f0c3207bfe06c172b9
alayor <alayor3@gmail.com>
2018-10-07 13:24:29 -0600

    Add logo.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='content'>
+        <img src='logo.jpg' className='logo'/>
         <button>Ordenar por WhatsApp</button>
       </div>
     );
